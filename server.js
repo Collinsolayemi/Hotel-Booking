fecedc2a3d27d5d68c40ac64e0235be5802bee25
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-18 18:03:19 -0700

    implementing installed dependencies

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-const express = require('express')
-const dotenv = require('dotenv')
-dotenv.config({ path: './env'})
+
+import express from 'express'
+import dotenv from 'dotenv'
+import bcrypt from 'bcryptjs'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import jsonwebtoken from 'jsonwebtoken'
+
+const app = express()
