776b056b48dcfd4fe9aa5233574c0cb73f0cbc2b
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-22 16:22:14 -0700

    verify user

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -9,5 +9,18 @@ export const verifyToken = (req, res) => {
 
   jwt.verify(token, process.env.JWT, (err, data) => {
     if (err) return next(createError(403, 'Token is not valid'));
+
+    req.user = user;
+    next();
+  });
+};
+
+const verifyUser = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.id === req.params.id || isAdmin) {
+      next();
+    } else {
+      return next(createError(401, 'You are not authorize'));
+    }
   });
 };
