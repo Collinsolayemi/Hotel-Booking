57ecaa01fb694f4896a43f6ef9fce4d57f833977
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-31 12:26:19 -0700

    delete user

--- controllers/user.js
+++ controllers/user.js
@@ -11,3 +11,12 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const deleteUser = await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('user have been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
