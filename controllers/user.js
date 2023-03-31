60bbc43e6890196f862ea746496f2c491619e269
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-31 12:10:06 -0700

    update user

--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,13 @@
 const { User } = require('../models/user');
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const update = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    if (!update) {
+    }
+
+    res.status(200).json(update);
+  } catch (error) {
+    next(error);
+  }
+};
