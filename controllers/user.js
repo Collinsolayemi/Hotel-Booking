a0d66532617940e308fb7d8981f57f0ea7d7dafa
Collinsolayemi <collinsolayemi@gmail.com>
2023-03-31 12:27:49 -0700

    get user

--- controllers/user.js
+++ controllers/user.js
@@ -20,3 +20,12 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
